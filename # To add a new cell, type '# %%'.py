68e05c9cfc387c68d9904e3576fa5a77d1dbf543
Bostjan32 <spjeh.perkan@gmail.com>
2020-05-26 10:13:16 +0200

    brba

--- # To add a new cell, type '# %%'.py
+++ # To add a new cell, type '# %%'.py
@@ -23,4 +23,34 @@ for jak,avg in baza[["listed_in"]].iterrows():
 len(seznam.keys())
 
 # %%
+bb = baza.sort_values(by=['numVotes'], ascending=False)
 
+# %%
+najboljpopularni=bb.head(100)
+
+# %%
+from Orange.data.pandas_compat import table_from_frame
+import scipy.cluster.hierarchy as sch
+import matplotlib.pyplot as plt
+# %%
+brek=[]
+for index, row in najboljpopularni.iterrows():
+    for zanr in row['listed_in'].split(", "):
+        najboljpopularni.loc[index, zanr] = int(1)
+najboljpopularni = najboljpopularni.drop(columns=[ 'type', 'show_id', 'director', 'cast','country','date_added','release_year','rating','duration','listed_in','description','averageRating','numVotes','month_added','year_added']).fillna(0)
+data = table_from_frame(najboljpopularni)
+
+
+# %%
+X = data.X
+Y = data.Y
+
+L = sch.linkage(X, method="average", metric="cityblock")
+
+plt.figure(figsize=(4, 15))
+labels = [row["title"].value for row in data]
+D      = sch.dendrogram(L, labels=labels, leaf_font_size=7, orientation='left')
+plt.xlabel("Razdalja")
+plt.show()
+
+# %%
